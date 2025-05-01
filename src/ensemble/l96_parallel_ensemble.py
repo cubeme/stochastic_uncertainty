feat/fix: fix tendency computation when using scipy solver; add functionality to choose scipy solver method; create initial states individually

src/ensemble/l96_parallel_ensemble.py
@@ -14,7 +14,54 @@ import numpy as np
 from models.lorenz96_model import L96
 
 
-def run_single_l96_simulation(args):
+def run_single_l96_solver(args):
+    """
+    Run a single L96 simulation for a given initial state.
+
+    Args:
+        args (tuple): A tuple containing:
+            - x_init (np.ndarray): Initial conditions for the slow variables (X).
+            - y_init (np.ndarray): Initial conditions for the fast variables (Y).
+            - t_init (float): Initial time.
+            - k (int): Number of slow variables (X).
+            - j (int): Number of fast variables (Y) per slow variable.
+            - f (float): Forcing term.
+            - h (float): Coupling coefficient.
+            - b (float): Spatial-scale ratio.
+            - c (float): Time-scale ratio.
+            - si (float): Sampling interval.
+            - t_total (float): Total simulation time.
+            - solver (str): Scipy solver used for integration. 
+            - solver_method (str): Solver method used for integration (e.g., 'RK45').
+
+    Returns:
+        tuple: A tuple containing:
+            - x (np.ndarray): Array of slow variables (X) over time.
+            - y (np.ndarray): Array of fast variables (Y) over time.
+            - t (np.ndarray): Array of time points.
+    """
+    # init_state, config = args
+    # x_init, y_init, t_init = init_state
+    x_init, y_init, t_init, k, j, f, h, b, c, si, t_total, solver, solver_method = args
+
+    # Initialize the L96 model
+    # Seed is irrelevant, as initial state gets set
+    model = L96(k, j, f, h, b, c, seed=0)
+    model.set_state(x_init, y_init, t_init)
+
+    # Run the simulation
+    x, y, t, u = model.run(
+        si=si,
+        t_total=t_total,
+        solver=solver,
+        solver_method=solver_method,
+        return_coupling=True
+    )
+
+    return x, y, t, u
+
+
+def run_single_l96_manual(args):
     """
     Run a single L96 simulation for a given initial state.
 
@@ -32,8 +79,6 @@ def run_single_l96_simulation(args):
             - si (float): Sampling interval.
             - t_total (float): Total simulation time.
             - dt (float): Time step for numerical integration.
-            - method (str): Integration method (e.g., 'RK4').
-            - seed (int): Random seed for reproducibility.
 
     Returns:
         tuple: A tuple containing:
@@ -43,10 +88,11 @@ def run_single_l96_simulation(args):
     """
     # init_state, config = args
     # x_init, y_init, t_init = init_state
-    x_init, y_init, t_init, k, j, f, h, b, c, si, t_total, dt, method, seed = args
+    x_init, y_init, t_init, k, j, f, h, b, c, si, t_total, dt = args
 
     # Initialize the L96 model
-    model = L96(k, j, f, h, b, c, seed=seed)
+    # Seed is irrelevant, as initial state gets set
+    model = L96(k, j, f, h, b, c, seed=0)
     model.set_state(x_init, y_init, t_init)
 
     # Run the simulation
@@ -54,14 +100,15 @@ def run_single_l96_simulation(args):
         si=si,
         t_total=t_total,
         dt=dt,
-        method=method,
+        solver='manual',
         return_coupling=True
     )
 
     return x, y, t, u
 
 
-def run_l96_parallel(init_states, config, num_processes=mp.cpu_count(), seeds=None):
+def run_l96_parallel(x_init_states, y_init_states, t_init_states, config,
+                     perturb=False, seed=None, num_processes=mp.cpu_count()):
     """
     Run multiple L96 simulations in parallel using multiprocessing.
 
@@ -76,11 +123,13 @@ def run_l96_parallel(init_states, config, num_processes=mp.cpu_count(), seeds=No
             - 'c' (float): Time-scale ratio.
             - 'si' (float): Sampling interval.
             - 't_total' (float): Total simulation time.
-            - 'dt' (float): Time step for numerical integration.
-            - 'method' (str): Integration method (e.g., 'RK4').
-            - 'seed' (int): Random seed for reproducibility.
+            - 'solver' (str): Integration scheme ('manual', 'solve_ivp' or 'odeint').
+            - 'solver_method' (str): Integration method when using scipy solver 'solve_ivp'. 
+            - 'dt' (float): Time step for numerical integration when using manual integration. 
+        perturb (bool, optional): Whether to perturb the initial states. Defaults to False.
+        seed (int, optional): Random seed used for perturbation of initial states. 
+            Must be set when `perturb=True`. Defaults to None. 
         num_processes (int, optional): Number of processes to use. Defaults to the number of CPU cores.
-        seeds (list, optional): List of random seeds for each simulation. If provided, each simulation gets a unique seed.
 
     Returns:
         tuple: A tuple containing:
@@ -89,22 +138,34 @@ def run_l96_parallel(init_states, config, num_processes=mp.cpu_count(), seeds=No
             - t_per_state (np.ndarray): Array of time points for all states.
             - u_per_state (np.ndarray): Coupling term history for all states.
     """
-    # todo: which seed version should be use? 
-    if seeds is None:
-        # Every L96 model gets the same seed
-        tasks = [(x_init, y_init, t_init, config['K'], config['J'], config['F'],
-                  config['h'], config['b'], config['c'], config['si'], config['t_total'],
-                  config['dt'], config['method'], config['seed']) for (x_init, y_init, t_init) in init_states]
-    else:
-        # Every L96 model gets a different seed
-        # Should be just one init state in this case
-        tasks = [(x_init, y_init, t_init, config['K'], config['J'], config['F'],
-                  config['h'], config['b'], config['c'], config['si'], config['t_total'],
-                  config['dt'], config['method'], seed) for (x_init, y_init, t_init) in init_states for seed in seeds]
+    if perturb and seed is None:
+        raise ValueError(
+            "Random seed is required to perturb initial states.")
 
-    # Use multiprocessing to run simulations in parallel
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(run_single_l96_simulation, tasks)
+    if perturb:
+        rg = np.random.default_rng(seed=seed)
+        x_init_states += rg.normal(0, 1, size=x_init_states.shape)
+
+    if config['solver'] == 'manual':
+
+        tasks = [(x_init_states[i], y_init_states[i], t_init_states[i],
+                  config['K'], config['J'], config['F'], config['h'], config['b'],
+                  config['c'], config['si'], config['t_total'], config['dt'])
+                 for i in range(t_init_states.shape[0])]
+
+        # Use multiprocessing to run simulations in parallel
+        with Pool(processes=num_processes) as pool:
+            results = pool.map(run_single_l96_manual, tasks)
+
+    else:
+        tasks = [(x_init_states[i], y_init_states[i], t_init_states[i],
+                  config['K'], config['J'], config['F'], config['h'], config['b'],
+                  config['c'], config['si'], config['t_total'],
+                  config['solver'], config['solver_method']) for i in range(t_init_states.shape[0])]
+
+        # Use multiprocessing to run simulations in parallel
+        with Pool(processes=num_processes) as pool:
+            results = pool.map(run_single_l96_solver, tasks)
 
     # Combine results
     x_per_state, y_per_state, t_per_state, u_per_state = zip(*results)

src/ensemble/stochastic_poly_ensemble.py
@@ -14,7 +14,7 @@ import multiprocessing as mp
 import numpy as np
 
 
-def run_stochastic_member(args):
+def run_stochastic_member_solver(args):
     """
     Run a single ensemble member using a GCM with stochastic parameterization.
 
@@ -38,12 +38,13 @@ def run_stochastic_member(args):
             - x_pred (np.ndarray): Predicted state variables over time.
             - t (np.ndarray): Corresponding time points.
     """
-    i_init, i_member, init_state, f, si, t_total, coefs, phi, sigma_e, seed = args
+    i_init, i_member, init_state, f, si, t_total, coefs, phi, sigma_e, solver, solver_method, seed = args
 
     stoch_param = PolynomialAR1Parameterization(coefs, phi, sigma_e, seed=seed)
     gcm_model = GCM(f, stoch_param)
 
-    x_pred, t = gcm_model(init_state, si=si, t_total=t_total)
+    x_pred, t = gcm_model(init_state, si=si, t_total=t_total,
+                          solver=solver, solver_method=solver_method)
 
     return i_init, i_member, x_pred, t
 
@@ -83,8 +84,9 @@ def run_stochastic_member_manual(args):
     return i_init, i_member, x_pred, t
 
 
-def run_stochastic_ensemble_parallel_multiprocessing(n_init_states, n_ens, step_size_init_states,
-                                                     x_true, t_total, f, si, k, member_func, seeds, perturb, *args):
+def run_stochastic_ensemble_parallel_multiprocessing(n_init_states, n_ens, init_states, t_total,
+                                                     f, si, k, member_func,  coefs, phi, sigma_e,
+                                                     seeds, perturb=False, *args):
     """
     Run an ensemble forecast using the given GCM model in parallel with multiprocessing.
 
@@ -97,10 +99,17 @@ def run_stochastic_ensemble_parallel_multiprocessing(n_init_states, n_ens, step_
         f (float): Forcing term for the GCM.
         si (float): Sampling interval.
         k (int): Number of state variables.
-        member_func (callable): Function to run a single ensemble member.
+        member_func (callable): Function to run a single ensemble member. Options: 
+            - `run_stochastic_member_solver`: Runs a single ensemble member using a GCM using scipy solver time stepping.
+            - `run_stochastic_member_manual`: Runs a single ensemble member using a manually stepped GCM.
         seeds (list): List of random seeds for each simulation.
         perturb (bool): Whether to perturb the initial states.
-        *args: Additional arguments required by the `member_func`.
+        *args: Additional arguments required by the `member_func`. 
+            Options when using scipy solver (`member_func=run_stochastic_member_solver`)
+            - solver (str): Integration method for the GCM. Options are 'solve_ivp' or 'odeint'.
+            - solver_method (str): Method to use with the 'solve_ivp' solver. Default is 'RK45'.
+            Options when using manual solver (`member_func=run_stochastic_member_manual`)
+            - dt (float): Time step for numerical integration (used with manual integration).
 
     Returns:
         tuple: A tuple containing:
@@ -115,21 +124,14 @@ def run_stochastic_ensemble_parallel_multiprocessing(n_init_states, n_ens, step_
     nt = int(t_total / si)
     x_ens_forecast = np.zeros([n_init_states, n_ens, nt + 1, k])
     t_ens_forecast = np.zeros([n_init_states, n_ens, nt + 1])
-
-    init_state_indices = np.arange(
-        0, n_init_states * step_size_init_states, step_size_init_states, dtype=int)
-
-    init_states = [x_true[init_state_indices[i_init]]
-                   for i_init in range(n_init_states)]
-
+    
     if perturb:
         rg = np.random.default_rng(seed=seeds[-1]+1)
-        init_states = [
-            i_state + rg.normal(0, 1, size=i_state.shape[0]) for i_state in init_states]
+        init_states += rg.normal(0, 1, size=init_states.shape)
 
     # Prepare arguments for each process
-    tasks = [(i_init, i_member, i_state, f, si, t_total, *args)
-             for i_init, i_state in enumerate(init_states) for i_member in range(n_ens)]
+    tasks = [(i_init, i_member, init_states[i_init], f, si, t_total, coefs, phi, sigma_e, *args)
+             for i_init in range(n_init_states) for i_member in range(n_ens)]
     tasks = [(*task_tuple, s) for task_tuple, s in zip(tasks, seeds)]
 
     # Use multiprocessing Pool for parallel execution
@@ -141,4 +143,4 @@ def run_stochastic_ensemble_parallel_multiprocessing(n_init_states, n_ens, step_
         x_ens_forecast[i_init, i_member] = x_pred
         t_ens_forecast[i_init, i_member] = t
 
-    return x_ens_forecast, t_ens_forecast, init_state_indices
+    return x_ens_forecast, t_ens_forecast

src/models/gcm_models.py
@@ -9,6 +9,7 @@ Functions:
 - L96_eq1_x_dot: Computes the time rate of change for the X variables in the Lorenz '96 model.
 """
 
+import logging
 import numpy as np
 from numba import njit
 from scipy.integrate import odeint, solve_ivp
@@ -61,6 +62,7 @@ class GCM:
         """
         self.f = f
         self.parameterization = parameterization
+        self.logger = logging.getLogger(__name__)
 
     def rhs(self, t: float, x: np.ndarray) -> np.ndarray:
         """
@@ -75,7 +77,7 @@ class GCM:
         """
         return L96_eq1_x_dot(x, self.f) + self.parameterization(x)
 
-    def __call__(self, x_init: np.ndarray, si: float, t_total: float, method: str = 'solve_ivp') -> Tuple[np.ndarray, np.ndarray]:
+    def __call__(self, x_init: np.ndarray, si: float, t_total: float, solver: str = 'solve_ivp', solver_method: str = 'RK45') -> Tuple[np.ndarray, np.ndarray]:
         """
         Integrate the system forward in time.
 
@@ -86,16 +88,20 @@ class GCM:
             si (float): Sampling interval (time increment for each step).
             t_total (float): Total simulation time.
             method (str): Numerical solver to use ('solve_ivp' or 'odeint'). Defaults to 'solve_ivp'.
+            solver_method (str, optional): Method to use with the 'solve_ivp' solver. Default is 'RK45'.
 
         Returns:
             Tuple[np.ndarray, np.ndarray]: A tuple containing:
                 - hist (np.ndarray): History of the large-scale state over time. Shape: (nt + 1, K).
                 - time (np.ndarray): Array of time points corresponding to the simulation. Shape: (nt + 1,).
         """
-        implemented_methods = ['solve_ivp', 'ode_int']
-        if method not in implemented_methods:
+        implemented_methods = ['solve_ivp', 'odeint']
+        if solver not in implemented_methods:
             raise ValueError(
-                f"Unknown method: {method}. Method must be one of {implemented_methods}")
+                f"Unknown method: {solver}. Method must be one of {implemented_methods}")
+        if solver == 'odeint':
+            self.logger.info(
+                "`solver_method=%s` is only used with `solver=solve_ivp`.", solver_method)
 
         # Number of time steps
         nt = int(t_total / si)
@@ -107,16 +113,16 @@ class GCM:
         hist[0] = x_init
 
         # Solve the ODE using the specified RHS
-        if method == 'odeint':
+        if solver == 'odeint':
             ode_result = odeint(self.rhs, y0=x_init, t=t_eval, tfirst=True)
 
             hist[1:] = ode_result
             # Simulation time
             time = np.arange(0, t_total + si, step=si)
 
-        elif method == 'solve_ivp':
+        elif solver == 'solve_ivp':
             ode_result = solve_ivp(
-                self.rhs, (0, nt), y0=x_init, method='RK45', t_eval=t_eval)
+                self.rhs, (0, nt), y0=x_init, method=solver_method, t_eval=t_eval)
 
             hist[1:] = np.swapaxes(ode_result.y, 0, 1)
             # Simulation time
@@ -198,5 +204,3 @@ class GCMManual:
             hist[n + 1], time[n + 1] = x, dt * (n + 1)
 
         return hist, time
-
-

src/models/lorenz96_model.py
@@ -1,4 +1,3 @@
-
 """
 Provides implementations of the Lorenz '96 model, including
 a two-time-scale version with slow (X) and fast (Y) variables.
@@ -6,13 +5,16 @@ a two-time-scale version with slow (X) and fast (Y) variables.
 Functions:
 - L96_2t_x_dot_y_dot_solver: Computes the time derivatives for the two-time-scale Lorenz '96 model using a solver-friendly format.
 - L96_2t_x_dot_y_dot_manual: Computes the time derivatives for the two-time-scale Lorenz '96 model manually.
+- calculate_xy_tendencies: Computes the coupling tendencies from fast Y variables.
 
 Classes:
 - L96: A class for simulating the two-time-scale Lorenz '96 model.
 """
+import logging
 import numpy as np
 from numba import njit
 from scipy.integrate import solve_ivp, odeint
+from torch import dtype
 
 
 @njit
@@ -35,9 +37,9 @@ def L96_2t_x_dot_y_dot_solver(t, xy_vars, K, J, F, h, b, c):
         c (float): Time-scale ratio.
 
     Returns:
-        np.ndarray: Concatenated array of time derivatives for X and Y variables, and the coupling term. Shape: (K + K*J + K,).
+        np.ndarray: Concatenated array of time derivatives for X and Y variables. Shape: (K + K*J,).
     """
-    x, y = xy_vars[:K], xy_vars[K:K+K*J]
+    x, y = xy_vars[:K], xy_vars[K:]
 
     # Helper variables
     hcb = (h * c) / b
@@ -53,7 +55,7 @@ def L96_2t_x_dot_y_dot_solver(t, xy_vars, K, J, F, h, b, c):
         - c * y + hcb * np.repeat(x, J)
     )
 
-    return np.concatenate((x_dot, y_dot, -hcb * y_summed))
+    return np.concatenate((x_dot, y_dot))
 
 
 @njit
@@ -102,6 +104,32 @@ def L96_2t_x_dot_y_dot_manual(x, y, F, h, b, c):
     return x_dot, y_dot, -hcb * y_summed
 
 
+def calculate_xy_tendencies(y_hist, h, c, b, k, j, nt):
+    """
+    Compute the coupling tendencies for the Lorenz '96 model from small-scale Y variables.
+
+    Args:
+        y_hist (np.ndarray): History of the fast variables (Y) over time. Shape: (nt+1, K*J).
+        h (float): Coupling coefficient.
+        c (float): Time-scale ratio.
+        b (float): Ratio of amplitudes.
+        k (int): Number of slow variables (X).
+        j (int): Number of fast variables (Y) per slow variable.
+        nt (int): Number of time steps.
+
+    Returns:
+        np.ndarray: Coupling tendencies for the slow variables (X) over time. Shape: (nt+1, K).
+    """
+    xy_tend_hist = np.zeros((nt + 1, k))
+
+    hcb = (h * c) / b
+    y_summed = y_hist.reshape(
+        (y_hist.shape[:-1] + (k, j))).sum(axis=-1)
+    xy_tend_hist[1:] = -hcb * y_summed[:-1]
+
+    return xy_tend_hist
+
+
 class L96:
     """
     Class for the two time-scale Lorenz '96 model.
@@ -135,7 +163,9 @@ class L96:
             seed (int): Random seed for reproducibility. Default is 17.
         """
         np.random.seed(seed)
-        
+
+        self.logger = logging.getLogger(__name__)
+
         self.f, self.h, self.b, self.c = F, h, b, c
         # Initialize system state
         self.x, self.y, self.t = b * \
@@ -204,7 +234,7 @@ class L96:
             np.random.rand(self.x.size), np.random.rand(self.y.size)
         return self.set_state(X, Y)
 
-    def run(self, si, t_total, store=False, return_coupling=False, dt=0.001, method='manual'):
+    def run(self, si, t_total, store=False, return_coupling=False, dt=0.001, solver='manual', solver_method='RK45'):
         """
         Run the Lorenz '96 model for a total time `t_total`, sampling at intervals of `si`.
 
@@ -214,8 +244,9 @@ class L96:
             store (bool, optional): If True, stores the final state as the initial conditions for the next run.
             return_coupling (bool, optional): If True, returns the coupling term in addition to X, Y, and time.
             dt (float, optional): Time step for numerical integration. Default is 0.001. Only used with manual integration.
-            method (str, optional): Integration method. Options are 'solve_ivp', 'ode_int', or 'manual'.
+            solver (str, optional): Integration method. Options are 'solve_ivp', 'ode_int', or 'manual'.
                                     Default is 'manual'.
+            solver_method (str, optional): Method to use with the 'solve_ivp' solver. Default is 'RK45'.
 
         Returns:
             tuple:
@@ -225,59 +256,66 @@ class L96:
                 - xy_tend_hist (numpy.ndarray, optional): Coupling term history (if `return_coupling=True`).
         """
 
-        implemented_methods = ['solve_ivp', 'ode_int', 'manual']
-        if method not in implemented_methods:
+        implemented_methods = ['solve_ivp', 'odeint', 'manual']
+        if solver not in implemented_methods:
             raise ValueError(
-                f"Unknown method: {method}. Method must be one of {implemented_methods}")
+                f"Unknown method: {solver}. Method must be one of {implemented_methods}")
+        if solver == 'manual' or solver == 'odeint':
+            self.logger.info(
+                "`solver_method=%s` is only used with `solver='solve_ivp'`.", solver_method)
+        if solver == 'solve_ivp' or solver == 'odeint':
+            self.logger.info(
+                "`dt=%f` is only used with `solver='manual'`.", dt)
 
         # Number of time steps
         nt = int(t_total / si)
 
-        x_0 = self.x
-        y_0 = self.y
+        # todo: remove copy() this after debugging
+        x_0 = self.x.copy()
+        y_0 = self.y.copy()
         t_0 = self.t
 
         # Result arrays
-        x_hist, y_hist, xy_tend_hist = (
+        x_hist, y_hist = (
             np.zeros((nt + 1, len(x_0))),
             np.zeros((nt + 1, len(y_0))),
-            np.zeros((nt + 1, len(x_0))),
         )
 
         x_hist[0, :] = x_0.copy()
         y_hist[0, :] = y_0.copy()
-        xy_tend_hist[0, :] = 0
 
         # Evaluation interval with si size steps
-        t_eval = np.arange(0, si * nt, si)
+        t_eval = np.arange(si, (si * nt) + si, si) + t_0
 
-        if method == 'solve_ivp':
+        if solver == 'solve_ivp':
             # Using solve_ivp
             ode_result = solve_ivp(L96_2t_x_dot_y_dot_solver, t_span=(0, nt),
-                                   y0=np.concatenate((x_0, y_0, np.zeros(self.k,))), method='RK45',
-                                   t_eval=t_eval, args=(
-                self.k, self.j, self.f, self.h, self.b, self.c))
+                                   y0=np.concatenate((x_0, y_0)), method=solver_method,
+                                   t_eval=t_eval, args=(self.k, self.j, self.f, self.h, self.b, self.c))
 
             # Collect results
             x_hist[1:] = np.swapaxes(ode_result.y[:self.k], 0, 1)
-            y_hist[1:] = np.swapaxes(ode_result.y[self.k:self.k+self.jk], 0, 1)
-            xy_tend_hist[1:] = np.swapaxes(ode_result.y[self.k+self.jk:], 0, 1)
+            y_hist[1:] = np.swapaxes(ode_result.y[self.k:], 0, 1)
+
+            xy_tend_hist = calculate_xy_tendencies(
+                y_hist, self.h, self.c, self.b, self.k, self.j, nt)
 
             time = t_0 + np.zeros((nt + 1))
             time[1:] = ode_result.t
 
-        elif method == 'ode_int':
+        elif solver == 'odeint':
             # Using odeint
-            ode_result = odeint(L96_2t_x_dot_y_dot_manual,  y0=np.concatenate((x_0, y_0, np.zeros(self.k,))),
-                                t=t_eval, args=(
-                                    self.k, self.j, self.f, self.h, self.b, self.c),
-                                tfirst=True)
+            ode_result = odeint(L96_2t_x_dot_y_dot_solver,  y0=np.concatenate((x_0, y_0)), t=t_eval,
+                                args=(self.k, self.j, self.f, self.h, self.b, self.c), tfirst=True)
             # Collect results
             x_hist[1:] = ode_result[:, :self.k]
-            y_hist[1:] = ode_result[:, self.k:self.k+self.jk]
-            xy_tend_hist[1:] = ode_result[:, self.k+self.jk:]
+            y_hist[1:] = ode_result[:, self.k:]
+
+            xy_tend_hist = calculate_xy_tendencies(
+                y_hist, self.h, self.c, self.b, self.k, self.j, nt)
 
             time = np.arange(t_0, t_total+t_0+si, step=si)
+
         else:  # manual
             if si < dt:
                 dt, ns = si, 1
@@ -290,7 +328,7 @@ class L96:
             x = x_0.copy()
             y = y_0.copy()
             time = t_0 + np.zeros((nt + 1))
-            
+
             for n in range(nt):
                 for s in range(ns):
                     # RK4 update of X,Y

src/utils/saving_and_loading.py
@@ -21,14 +21,14 @@ def save_output_l96(output_folder, config, x, y, t, u, extra_ident=""):
     """
     Save Lorenz '96 simulation results to the specified output folder.
 
-    The results are saved in .npy format the following directory structure:
-    `output_folder`/
-        L96/
-            c{c}_dt{dt}_si{si}_time{t_total}/
-                x.npy
-                y.npy
-                t.npy
-                u.npy
+    The results are saved in .npy format in the following directory structure:
+        `output_folder`/
+            L96/
+                c{c}_dt{dt}_si{si}_time{t_total}/
+                    x.npy
+                    y.npy
+                    t.npy
+                    u.npy
 
     Args:
         output_folder (str): Path to the output folder.
@@ -125,6 +125,7 @@ def save_output_gcm(output_folder, config, parameterization, x, t, extra_ident="
             - 'seed' (int): Random seed for reproducibility.
         parameterization (str): Name of the parameterization used in the simulation.
         x (numpy.ndarray): Array of state variables.
+        t (numpy.ndarray): Array of time points.
         extra_ident (str, optional): Additional identifier for the folder. Default is an empty string.
     """
     f_ident = (
@@ -187,7 +188,7 @@ def load_output_gcm(output_folder, config, parameterization, extra_ident=""):
     return x, t
 
 
-def save_output_ensemble(output_folder, config, parameterization, x, t, init_idx, seeds, extra_ident=""):
+def save_output_ensemble(output_folder, config, parameterization, x, t, seeds, extra_ident=""):
     """
     Save ensemble simulation results to the specified output folder.
 
@@ -208,11 +209,10 @@ def save_output_ensemble(output_folder, config, parameterization, x, t, init_idx
             - 't_total' (float): Total simulation time.
             - 'init_states' (int): Number of initial states.
             - 'n_ens' (int): Number of ensemble members.
-            - 'seed' (int): Random seed for reproducibility.
         parameterization (str): Name of the parameterization used in the simulation.
         x (numpy.ndarray): Array of ensemble state variables.
         t (numpy.ndarray): Array of time points.
-        init_idx (numpy.ndarray): Array of initial state indices.
+        seeds (list): List of random seeds used for reproducibility.
         extra_ident (str, optional): Additional identifier for the folder. Default is an empty string.
     """
     seed_str = f"{seeds[0]}-{seeds[-1]}"
@@ -232,7 +232,6 @@ def save_output_ensemble(output_folder, config, parameterization, x, t, init_idx
 
     np.save(os.path.join(output_path, "x.npy"), x)
     np.save(os.path.join(output_path, "t.npy"), t)
-    np.save(os.path.join(output_path, "init_indices.npy"), init_idx)
 
 
 def load_output_ensemble(output_folder, config, parameterization, seeds, extra_ident=""):
@@ -257,8 +256,8 @@ def load_output_ensemble(output_folder, config, parameterization, seeds, extra_i
             - 't_total' (float): Total simulation time.
             - 'init_states' (int): Number of initial states.
             - 'n_ens' (int): Number of ensemble members.
-            - 'seed' (int): Random seed for reproducibility.
         parameterization (str): Name of the parameterization used in the simulation.
+        seeds (list): List of random seeds used for reproducibility.
         extra_ident (str, optional): Additional identifier for the folder. Default is an empty string.
 
     Returns:
@@ -268,6 +267,7 @@ def load_output_ensemble(output_folder, config, parameterization, seeds, extra_i
             - init_idx (numpy.ndarray): Array of initial state indices.
     """
     seed_str = f"{seeds[0]}-{seeds[-1]}"
+    
     f_ident = (
         f"c{config['c']}_"
         f"dt{config['dt']}_"
@@ -282,12 +282,11 @@ def load_output_ensemble(output_folder, config, parameterization, seeds, extra_i
 
     x = np.load(os.path.join(save_folder, "x.npy"))
     t = np.load(os.path.join(save_folder, "t.npy"))
-    init_indices = np.load(os.path.join(save_folder, "init_indices.npy"))
 
-    return x, t, init_indices
+    return x, t
 
 
-def save_output_l96_ensemble_simulation(output_folder, config, x, y, t, u, seeds=None, extra_ident=""):
+def save_output_l96_ensemble_simulation(output_folder, config, x, y, t, u, seeds, extra_ident=""):
     """
     Save ensemble simulation results to the specified output folder.
 
@@ -308,17 +307,14 @@ def save_output_l96_ensemble_simulation(output_folder, config, x, y, t, u, seeds
             - 't_total' (float): Total simulation time.
             - 'init_states' (int): Number of initial states.
             - 'n_ens' (int): Number of ensemble members.
-            - 'seed' (int): Random seed for reproducibility.
-        parameterization (str): Name of the parameterization used in the simulation.
-        x (numpy.ndarray): Array of ensemble state variables.
+        x (numpy.ndarray): Array of slow variables (X).
+        y (numpy.ndarray): Array of fast variables (Y).
         t (numpy.ndarray): Array of time points.
-        init_idx (numpy.ndarray): Array of initial state indices.
+        u (numpy.ndarray): Coupling term history.
+        seeds (list): List of random seeds used for reproducibility.
         extra_ident (str, optional): Additional identifier for the folder. Default is an empty string.
     """
-    if seeds is not None:
-        seed_str = f"{seeds[0]}-{seeds[-1]}"
-    else:
-        seed_str = config['seed']
+    seed_str = f"{seeds[0]}-{seeds[-1]}"
 
     f_ident = (
         f"c{config['c']}_"
@@ -339,18 +335,20 @@ def save_output_l96_ensemble_simulation(output_folder, config, x, y, t, u, seeds
     np.save(os.path.join(output_path, "u.npy"), u)
 
 
-def load_output_l96_ensemble_simulation(output_folder, config, seeds=None, extra_ident=""):
+def load_output_l96_ensemble_simulation(output_folder, config, seeds, extra_ident=""):
     """
-    Save ensemble simulation results to the specified output folder.
+    Load ensemble simulation results from the specified output folder.
 
-    The results are saved in .npy format in the following directory structure:
+    The results are loaded from the following directory structure:
         `output_folder`/
             ensemble/
-                `parameterization`/
+                l96/
                     c{c}_dt{dt}_si{si}_time{t_total}_init{init_states}_ens{n_ens}_rs{seed}/
                         x.npy
+                        y.npy
                         t.npy
-                        init_indices.npy
+                        u.npy
+
     Args:
         output_folder (str): Path to the output folder.
         config (dict): Configuration dictionary containing simulation parameters. Must include:
@@ -360,11 +358,7 @@ def load_output_l96_ensemble_simulation(output_folder, config, seeds=None, extra
             - 't_total' (float): Total simulation time.
             - 'init_states' (int): Number of initial states.
             - 'n_ens' (int): Number of ensemble members.
-            - 'seed' (int): Random seed for reproducibility.
-        parameterization (str): Name of the parameterization used in the simulation.
-        x (numpy.ndarray): Array of ensemble state variables.
-        t (numpy.ndarray): Array of time points.
-        init_idx (numpy.ndarray): Array of initial state indices.
+        seeds (list): List of random seeds used for reproducibility.
         extra_ident (str, optional): Additional identifier for the folder. Default is an empty string.
 
     Returns:
@@ -373,12 +367,8 @@ def load_output_l96_ensemble_simulation(output_folder, config, seeds=None, extra
             - y (numpy.ndarray): Array of fast variables (Y).
             - t (numpy.ndarray): Array of time points.
             - u (numpy.ndarray): Coupling term history.
-
     """
-    if seeds is not None:
-        seed_str = f"{seeds[0]}-{seeds[-1]}"
-    else:
-        seed_str = config['seed']
+    seed_str = f"{seeds[0]}-{seeds[-1]}"
 
     f_ident = (
         f"c{config['c']}_"